4434384d48e7a59986d9fdd059682971c7b79a79
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2022-05-19 19:34:14 -0300

    GeneroRecurso esta pronto

--- src/main/java/SpringBoot/recursos/GeneroRecurso.java
+++ src/main/java/SpringBoot/recursos/GeneroRecurso.java
@@ -1,4 +1,45 @@
 package SpringBoot.recursos;
 
+import SpringBoot.entidades.Genero;
+import SpringBoot.repositorios.AnaliseRepositorio;
+import SpringBoot.repositorios.CompraRepositorio;
+import SpringBoot.repositorios.DesenvolvedorRepositorio;
+import SpringBoot.repositorios.GeneroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/generos")
 public class GeneroRecurso {
+
+    @Autowired
+    private GeneroRepositorio generoRepositorio;
+
+    @GetMapping
+    public ResponseEntity<List<Genero>> buscarTudo() {
+
+        return ResponseEntity.ok(generoRepositorio.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Genero> inserir(@RequestBody Genero genero) {
+        Genero retorno = generoRepositorio.save(genero);
+        return ResponseEntity.ok(retorno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable("id") Integer id) {
+        generoRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Genero> editar(@RequestBody Genero genero) {
+        Genero retorno = generoRepositorio.save(genero);
+        return ResponseEntity.ok(retorno);
+    }
+
 }
