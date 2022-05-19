720708b68ec0b070883402ecff13de1a611d46f2
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2022-05-19 19:19:31 -0300

    AnaliseRecurso Prontinho

--- src/main/java/SpringBoot/recursos/AnaliseRecurso.java
+++ src/main/java/SpringBoot/recursos/AnaliseRecurso.java
@@ -1,4 +1,42 @@
 package SpringBoot.recursos;
 
+import SpringBoot.entidades.Analise;
+import SpringBoot.repositorios.AnaliseRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/analises")
 public class AnaliseRecurso {
+
+    @Autowired
+    private AnaliseRepositorio analiseRepositorio;
+
+    @GetMapping
+    public ResponseEntity<List<Analise>> buscarTudo() {
+
+        return ResponseEntity.ok(analiseRepositorio.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Analise> inserir(@RequestBody Analise analise) {
+        Analise retorno = analiseRepositorio.save(analise);
+        return ResponseEntity.ok(retorno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable("id") Integer id) {
+        analiseRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Analise> editar(@RequestBody Analise analise) {
+        Analise retorno = analiseRepositorio.save(analise);
+        return ResponseEntity.ok(retorno);
+    }
+
 }
