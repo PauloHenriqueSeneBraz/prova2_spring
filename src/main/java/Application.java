c6e335c00a41d5695a22708811ebd41ed2ee16fa
Lucas Zanon <lucaspzanonn@outlook.com>
2022-05-19 18:50:15 -0300

    .
    .

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,11 @@
+package br.edu.unifio.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
