c16a17db7e69c25248136ac5975fabb8d4b417af
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2022-05-19 19:31:37 -0300

    DesenvolvedorRecurso ta pronto

--- src/main/java/SpringBoot/recursos/DesenvolvedorRecurso.java
+++ src/main/java/SpringBoot/recursos/DesenvolvedorRecurso.java
@@ -1,4 +1,44 @@
 package SpringBoot.recursos;
 
+import SpringBoot.entidades.Desenvolvedor;
+import SpringBoot.repositorios.AnaliseRepositorio;
+import SpringBoot.repositorios.CompraRepositorio;
+import SpringBoot.repositorios.DesenvolvedorRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/desenvolvedores")
 public class DesenvolvedorRecurso {
+
+    @Autowired
+    private DesenvolvedorRepositorio desenvolvedorRepositorio;
+
+    @GetMapping
+    public ResponseEntity<List<Desenvolvedor>> buscarTudo() {
+
+        return ResponseEntity.ok(desenvolvedorRepositorio.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Desenvolvedor> inserir(@RequestBody Desenvolvedor desenvolvedor) {
+        Desenvolvedor retorno = desenvolvedorRepositorio.save(desenvolvedor);
+        return ResponseEntity.ok(retorno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable("id") Integer id) {
+        desenvolvedorRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Desenvolvedor> editar(@RequestBody Desenvolvedor desenvolvedor) {
+        Desenvolvedor retorno = desenvolvedorRepositorio.save(desenvolvedor);
+        return ResponseEntity.ok(retorno);
+    }
+
 }
