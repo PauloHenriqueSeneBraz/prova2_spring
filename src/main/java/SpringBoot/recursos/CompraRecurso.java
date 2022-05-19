c39b6dd9e74008b18bc9ee70b5339c72f7ed0673
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2022-05-19 19:28:04 -0300

    CompraRecurso ta pronta

--- src/main/java/SpringBoot/recursos/CompraRecurso.java
+++ src/main/java/SpringBoot/recursos/CompraRecurso.java
@@ -1,4 +1,43 @@
 package SpringBoot.recursos;
 
+import SpringBoot.entidades.Compra;
+import SpringBoot.repositorios.AnaliseRepositorio;
+import SpringBoot.repositorios.CompraRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/compras")
 public class CompraRecurso {
+
+    @Autowired
+    private CompraRepositorio compraRepositorio;
+
+    @GetMapping
+    public ResponseEntity<List<Compra>> buscarTudo() {
+
+        return ResponseEntity.ok(compraRepositorio.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Compra> inserir(@RequestBody Compra compra) {
+        Compra retorno = compraRepositorio.save(compra);
+        return ResponseEntity.ok(retorno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable("id") Integer id) {
+        compraRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Compra> editar(@RequestBody Compra compra) {
+        Compra retorno = compraRepositorio.save(compra);
+        return ResponseEntity.ok(retorno);
+    }
+
 }
