7a13d6b90cf01c66bc611ee75cab2dc4a5008425
Renam Mello da Silva <57508500+renammello@users.noreply.github.com>
2022-05-19 19:39:28 -0300

    UsuarioRecurso esta pronto

--- src/main/java/SpringBoot/recursos/UsuarioRecurso.java
+++ src/main/java/SpringBoot/recursos/UsuarioRecurso.java
@@ -1,4 +1,42 @@
 package SpringBoot.recursos;
 
+import SpringBoot.entidades.Usuario;
+import SpringBoot.repositorios.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioRecurso {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarTudo() {
+
+        return ResponseEntity.ok(usuarioRepositorio.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> inserir(@RequestBody Usuario usuario) {
+        Usuario retorno = usuarioRepositorio.save(usuario);
+        return ResponseEntity.ok(retorno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluir(@PathVariable("id") Integer id) {
+        usuarioRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> editar(@RequestBody Usuario usuario) {
+        Usuario retorno = usuarioRepositorio.save(usuario);
+        return ResponseEntity.ok(retorno);
+    }
+
 }
